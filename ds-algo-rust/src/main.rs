0d85ee3eb45b02ac13ed60f2e68e36a8672810a2
Colin Graydon <colingraydon@gmail.com>
2024-06-11 23:18:08 -0400

    feat: finish vector. add iterator, insert/remove, shrink

--- ds-algo-rust/src/main.rs
+++ ds-algo-rust/src/main.rs
@@ -4,5 +4,13 @@ use vector::Vector;
 fn main() {
     let mut vec1: Vector<i32> = Vector::new();
     vec1.push(5);
-    print!("my vector {}", vec1);
+    vec1.push(5);
+    vec1.push(5);
+
+    vec1.insert(1, 4);
+    vec1.remove(0);
+
+    for elem in vec1 {
+        print!("elem is {}", elem);
+    }
 }
